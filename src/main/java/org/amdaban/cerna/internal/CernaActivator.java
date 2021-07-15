13d24986627ce8a0824077f029e46d0bc0791b01
Alireza Mohammadi <alireza.mohammadi@cafebazaar.ir>
2021-07-15 13:52:08 +0430

    Initialize cerna(ceRNA)

---
+++ src/main/java/org/amdaban/cerna/internal/CernaActivator.java
@@ -0,0 +1,21 @@
+package org.amdaban.cerna.internal;
+
+import org.cytoscape.service.util.AbstractCyActivator;
+import org.cytoscape.work.TaskFactory;
+
+import org.osgi.framework.BundleContext;
+
+import java.util.Properties;
+
+public class CernaActivator extends AbstractCyActivator {
+    @Override
+    public void start(BundleContext context) throws Exception {
+        CernaTaskFactory myFactory = new CernaTaskFactory();
+
+        Properties props = new Properties();
+        props.setProperty("preferredMenu", "Apps.cerna");
+        props.setProperty("title", "Generate ceRNA Network");
+
+        registerService(context, myFactory, TaskFactory.class, props);
+    }
+}
---
+++ src/main/java/org/amdaban/cerna/internal/CernaTaskFactory.java
@@ -0,0 +1,14 @@
+package org.amdaban.cerna.internal;
+
+import org.cytoscape.work.AbstractTaskFactory;
+import org.cytoscape.work.TaskIterator;
+
+class CernaTaskFactory extends AbstractTaskFactory {
+    public CernaTaskFactory() {
+        super();
+    }
+
+    public TaskIterator createTaskIterator() {
+        return null;
+    }
+}
